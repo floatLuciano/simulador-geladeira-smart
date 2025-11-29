8c5f0a4035ea0c5ead33b41d851e834a058f91bc
floatLuciano <lucianoandrade@id.uff.br>
2025-11-29 15:45:32 -0300

    eventos que aumentam ou diminuem o volume da geladeira

--- fisica.py
+++ fisica.py
@@ -20,6 +20,30 @@ class GeladeiraFisica:
         
         self.temp_ext = 30 - 10 * math.cos(tempo * 0.012)
 
+
+    def simular_eventos_usuario(self, tempo):
+        #Simula o comportamento humano: encher e esvaziar a geladeira. Isso altera o VOLUME e gera PICO DE CALOR (porta aberta). São eventos
+        #simples e predefinidos para testar cenários diferentes
+    
+        
+        # EVENTO 1: Chegada das compras na geladeira (Manhã - passo 100)
+        if tempo == 100:
+            print(f"\n[EVENTO {tempo}s] Compras na geladeira (fica cheia).")
+            self.volume = 9.0      # Vai para 90% de capacidade (Massa térmica alta)
+            self.temp_int += 2.0   # Porta aberta por muito tempo + alimentos quentes entrando
+            
+        # EVENTO 2: Preparando o almoço (Meio-dia - passo 250)
+        elif tempo == 250:
+            print(f"\n[EVENTO {tempo}s] Abrindo para pegar ingredientes.")
+            # Volume não muda muito, mas entra calor
+            self.temp_int += 2.0   
+            
+        # EVENTO 3: Faxina na geladeira / Jantar (Tarde - passo 400)
+        elif tempo == 400:
+            print(f"\n[EVENTO {tempo}s] Esvaziando a geladeira.")
+            self.volume = 2.0      # Cai para 20% (Vazia - esfria rápido, esquenta rápido)
+            self.temp_int += 1.0   # Porta aberta
+
     def simular_passo(self, potencia_aplicada, dt=1):
         #Aplica a Lei de Resfriamento de Newton via Integração de Euler. dt: passo de tempo da simulação (padrão = 1 unidade)
         # 1. Ganho de Calor (Troca com o ambiente)
--- main.py
+++ main.py
@@ -11,19 +11,19 @@ def rodar_simulacao():
     
     # 1. Instanciando os Módulos
     fuzzy_system = criar_controlador_fuzzy()
-    geladeira = GeladeiraFisica(temp_inicial=15.0, vol_inicial=8.0) # Começa quente (25°C) e Cheia (8.0)
+    geladeira = GeladeiraFisica(temp_inicial=10.0, vol_inicial=8.0) # Começa quente (25°C) e Cheia (8.0)
 
     # Cabeçalho da tabela
     print(f"{'TEMPO':<6} | {'T. EXT':<8} | {'T. INT':<8} | {'POTÊNCIA':<10} | {'STATUS'}")
     print("-" * 65)
 
     # 2. Loop de Simulação (Ex: 500 rodadas)
-    # Cada rodada representa um passo de tempo (ex: 30 segundos na vida real)
+    # Cada rodada representa um passo de tempo
     for t in range(500):
         
         # A. Atualiza o mundo (Física)
         geladeira.atualizar_ambiente_externo(t)
-        
+        geladeira.simular_eventos_usuario(t) 
         # B. Leitura dos Sensores -> Fuzzy
         fuzzy_system.input['temp_int'] = geladeira.temp_int
         fuzzy_system.input['temp_ext'] = geladeira.temp_ext
